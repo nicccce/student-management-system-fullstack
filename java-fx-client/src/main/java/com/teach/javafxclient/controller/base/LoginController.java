88ee782934801f510b3d4471ec2a3e369bb11a90
NickLin1024 <152240432+NickLin1024@users.noreply.github.com>
2024-05-13 21:29:24 +0800

    UPDATE
    没啥

--- java-fx-client/src/main/java/com/teach/javafxclient/controller/base/LoginController.java
+++ java-fx-client/src/main/java/com/teach/javafxclient/controller/base/LoginController.java
@@ -1,16 +1,27 @@
 package com.teach.javafxclient.controller.base;
 
 import com.teach.javafxclient.MainApplication;
+import com.teach.javafxclient.controller.demo.model.Model;
 import com.teach.javafxclient.request.*;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.dialogs.MFXGenericDialog;
+import io.github.palexdev.materialfx.dialogs.MFXGenericDialogBuilder;
+import io.github.palexdev.materialfx.dialogs.MFXStageDialog;
+import io.github.palexdev.materialfx.enums.ScrimPriority;
+import io.github.palexdev.mfxresources.fonts.MFXFontIcon;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * 登录交互控制类，用于处理登录界面的交互逻辑。
@@ -19,6 +30,9 @@ import java.io.IOException;
 public class LoginController {
     @FXML
     public Button loginButton;
+
+    @FXML
+    public AnchorPane anchorPane;
 
     /**
      * 用户名输入框的引用，对应FXML文件中的fx:id="usernameField"。
@@ -80,6 +94,30 @@ public class LoginController {
         usernameField.setText("admin");
         // usernameField.setText("2022030001"); // 注释掉的代码示例
         passwordField.setText("123456");
+        Platform.runLater(() -> {
+            this.dialogContent = MFXGenericDialogBuilder.build()
+                    .setContentText(Model.ipsum)
+                    .makeScrollable(true)
+                    .get();
+            this.dialog = MFXGenericDialogBuilder.build(dialogContent)
+                    .toStageDialogBuilder()
+                    .initOwner(stage)
+                    .initModality(Modality.APPLICATION_MODAL)
+                    .setDraggable(true)
+                    .setTitle("Dialogs Preview")
+                    .setOwnerNode(anchorPane)
+                    .setScrimPriority(ScrimPriority.WINDOW)
+                    .setScrimOwner(true)
+                    .get();
+
+            dialogContent.addActions(
+                    Map.entry(new MFXButton("Confirm"), event -> {
+                    }),
+                    Map.entry(new MFXButton("Cancel"), event -> dialog.close())
+            );
+
+            dialogContent.setMaxSize(400, 200);
+        });
     }
 
     /**
@@ -107,6 +145,19 @@ public class LoginController {
         }
     }
 
+
+    private MFXGenericDialog dialogContent;
+    private MFXStageDialog dialog;
+
+    private void convertDialogTo(String styleClass) {
+        dialogContent.getStyleClass().removeIf(
+                s -> s.equals("mfx-info-dialog") || s.equals("mfx-warn-dialog") || s.equals("mfx-error-dialog")
+        );
+
+        if (styleClass != null)
+            dialogContent.getStyleClass().add(styleClass);
+    }
+
     /**
      * 链接按钮点击事件处理方法。
      * 当前仅为示例，显示一个包含错误信息的对话框。
@@ -115,7 +166,11 @@ public class LoginController {
      */
     @FXML
     protected void linkButtonClick() throws IOException {
-        MessageDialog.showDialog("你的钢门比较松弛");//TODO: edit it!!!!
+        MFXFontIcon infoIcon = new MFXFontIcon("fas-circle-info", 18);
+        dialogContent.setHeaderIcon(infoIcon);
+        dialogContent.setHeaderText("你的钢门比较松弛");//TODO: edit it!!!!
+        convertDialogTo("mfx-info-dialog");
+        dialog.showDialog();
         return;
     }
 

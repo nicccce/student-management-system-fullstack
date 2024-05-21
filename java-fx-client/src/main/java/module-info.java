380644f70ca36dd3bfd31976efc97a73c67f3688
NickLin1024 <152240432+NickLin1024@users.noreply.github.com>
2024-05-21 14:48:40 +0800

    UPDATE
    新增了弹窗功能

--- java-fx-client/src/main/java/module-info.java
+++ java-fx-client/src/main/java/module-info.java
@@ -15,24 +15,27 @@ module com.teach.javafxclient {
     requires fr.brouillard.oss.cssfx;
     requires commons.beanutils;
     requires atlantafx.base;
+    requires fastjson;
 
 
     opens com.teach.javafxclient to javafx.fxml;
-    opens com.teach.javafxclient.model to javafx.base, commons.beanutils;
+    opens com.teach.javafxclient.model to javafx.base, commons.beanutils, fastjson, com.google.gson;
     exports com.teach.javafxclient;
     exports com.teach.javafxclient.controller;
     exports com.teach.javafxclient.controller.base;
     exports com.teach.javafxclient.request;
-    opens com.teach.javafxclient.request to com.google.gson, javafx.fxml;
+    opens com.teach.javafxclient.request to com.google.gson, javafx.fxml, fastjson;
     exports com.teach.javafxclient.util;
     exports com.teach.javafxclient.controller.admin;
     exports com.teach.javafxclient.controller.demo;
     exports com.teach.javafxclient.controller.demo.controllers;
     exports com.teach.javafxclient.controller.demo.model;
-    opens com.teach.javafxclient.util to com.google.gson, javafx.fxml;
-    opens com.teach.javafxclient.controller.base to com.google.gson, javafx.fxml;
-    opens com.teach.javafxclient.controller to com.google.gson, javafx.fxml;
-    opens com.teach.javafxclient.controller.admin to com.google.gson, javafx.fxml;
-    opens com.teach.javafxclient.controller.demo.controllers to com.google.gson, javafx.fxml;
-    opens com.teach.javafxclient.controller.demo to com.google.gson, javafx.fxml;
+    opens com.teach.javafxclient.util to com.google.gson, javafx.fxml, fastjson;
+    opens com.teach.javafxclient.controller.base to com.google.gson, javafx.fxml, fastjson;
+    opens com.teach.javafxclient.controller to com.google.gson, javafx.fxml, fastjson;
+    opens com.teach.javafxclient.controller.admin to com.google.gson, javafx.fxml, fastjson;
+    opens com.teach.javafxclient.controller.demo.controllers to com.google.gson, javafx.fxml, fastjson;
+    opens com.teach.javafxclient.controller.demo to com.google.gson, javafx.fxml, fastjson;
+    opens com.teach.javafxclient.controller.demo.model to com.google.gson, javafx.fxml, fastjson;
+    exports com.teach.javafxclient.model;
 }

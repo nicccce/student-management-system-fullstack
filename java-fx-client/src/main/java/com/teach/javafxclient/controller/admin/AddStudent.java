2914cb10f6051e99f66c58542f28ac3bb1eea79f
NickLin1024 <152240432+NickLin1024@users.noreply.github.com>
2024-05-21 16:28:18 +0800

    UPDATE
    添加了批量删除学生数据的前端

---
+++ java-fx-client/src/main/java/com/teach/javafxclient/controller/admin/AddStudent.java
@@ -0,0 +1,25 @@
+package com.teach.javafxclient.controller.admin;
+
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXDatePicker;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+
+public class AddStudent {
+
+    public MFXTextField numField;
+    public MFXTextField nameField;
+    public MFXTextField deptField;
+    public MFXTextField majorField;
+    public MFXTextField classNameField;
+    public MFXComboBox genderComboBox;
+    public MFXTextField cardField;
+    public MFXTextField emailField;
+    public MFXDatePicker birthdayPick;
+    public MFXTextField phoneField;
+    public MFXTextField addressField;
+
+    public void onSaveButtonClick(ActionEvent actionEvent) {
+
+    }
+}
